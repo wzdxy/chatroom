75cb3dcccf3450b4f750e6aa2203bdd76fc1bc81
ZC-GF <zc_93@qq.com>
2016-06-13 18:05:19 +0800

    响应式布局和回车发送

--- chat.js
+++ chat.js
@@ -1,28 +1,46 @@
 window.onload=function(){
             
-            ws = new WebSocket("ws://127.0.0.1:2346");
+            //ws = new WebSocket("ws://127.0.0.1:2346");
+            ws=new WebSocket("ws://192.168.3.76:2346");
             var id=Math.floor(Math.random()*99999+1);
             var nameInput=document.getElementById('name-input');
             nameInput.value='游客-' + id;
+            
             ws.onopen = function() {
                 userOnline(id,nameInput.value);
             };
+            
             ws.onmessage = function(e) {
                 innerChat(e);             
             }
+            
             textInput=document.getElementById('text-input');
             sentBtn=document.getElementById('sent-btn');
+            
+            textInput.addEventListener('keydown',function (event) {
+                if(event.keyCode==13){
+                    sendMessage(id,nameInput.value,textInput.value);
+                    textInput.value='';   
+                }
+            })
+            
+            
+            
             sentBtn.onclick=function () {
-                sendMessage(id,nameInput.value,textInput.value);    
+                if(textInput.value){
+                    sendMessage(id,nameInput.value,textInput.value);
+                    textInput.value='';    
+                }
+                    
             }
                         
        };
 function sendMessage(id,name,text) {
     var message=new Object();
     message.type='speak';
-    message.id=id;
+    //message.id=id;
     message.name=name;
-    message.text=text;
+    message.text=text;    
     var messageJSON=JSON.stringify(message);
     //alert(messageJSON);
     ws.send(messageJSON);
